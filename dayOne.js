ca06aec143dd604778ced77d16ace0edd485259a
NjeriMarion <njerimarion@gmail.comgit config --global user.email njerimarion@gmail.comhistorygit clone https://github.com/NjeriMarion/tools_new_2.git>
2023-04-09 21:34:53 +0300

    day One

---
+++ dayOne.js
@@ -0,0 +1,25 @@
+//  ********** Lecture1, Values and Variables ***************
+const country = "Kenya"
+const continent = "Africa"
+let population = 52000000
+
+console.log(`${country}, ${continent}, ${population}`)
+
+//  ********** Lecture2, Data Types ***************
+const isIsland = false
+let language = ``
+console.log(typeof(isIsland))
+console.log(typeof(language))
+
+//  ********** Lecture3, let, const and Var ***************
+language = "English"
+// country  = "Uganda"
+
+//  ********** Lecture4, Basic Operators ***************
+console.log("half of Kenya's popultion is ", population/2)
+console.log("Kenya's population increased by 1", population + 1)
+let finPopulation = 6000000
+console.log(population > finPopulation)
+let description = (`${country} is in ${continent}, and its ${population} people speak ${language}`)
+console.log( description)
+
